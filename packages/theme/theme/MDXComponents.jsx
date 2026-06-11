6e0c180509d85130f51c17353aa655aafec5c700
Soumadip Das <soumadip@zohomail.in>
2026-06-11 10:43:48 +0530

    fix(mdx): fix component file extension in imports

--- packages/theme/theme/MDXComponents.jsx
+++ packages/theme/theme/MDXComponents.jsx
@@ -2,8 +2,8 @@ import MDXComponents from "@theme-original/MDXComponents";
 import Details from "@theme/Details";
 import Tabs from "@theme/Tabs";
 import TabItem from "@theme/TabItem";
-import { Pv, SrcPv } from "./components/Preview/index.js";
-import Tooltip from "./components/Tooltip/index.js";
+import { Pv, SrcPv } from "./components/Preview/index.jsx";
+import Tooltip from "./components/Tooltip/index.jsx";
 import TopicList from "./components/TopicList/index.jsx";
 import Indent from "./components/Indent/index.jsx";
 
