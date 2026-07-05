dff2304d9d81556e4197c58e873924c796347926
Soumadip Das <soumadip@zohomail.in>
2026-07-06 02:15:40 +0530

    chore(release): 0.20.0

--- packages/theme/theme/DocCardList/index.jsx
+++ packages/theme/theme/DocCardList/index.jsx
@@ -1,5 +1,11 @@
 import TopicList from "@portosaur/theme/theme/components/TopicList/index.jsx";
 
 export default function DocCardListWrapper(props) {
-  return <TopicList forceRender={true} items={props.items} className={props.className} />;
+  return (
+    <TopicList
+      forceRender={true}
+      items={props.items}
+      className={props.className}
+    />
+  );
 }
